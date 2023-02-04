422c981eb8a52e97f525855f9f3a8cedc98362f5
AruMoon <anton17082003@gmail.com>
2023-02-04 21:35:38 +0500

    Revert "Resprite airlocks, firelocks, singularity and singularity field"
    This reverts commit 93e253bef18589e51e8b820daa44287daf3f13f9.

--- Content.Client/Doors/AirlockVisualizer.cs
+++ Content.Client/Doors/AirlockVisualizer.cs
@@ -184,8 +184,6 @@ namespace Content.Client.Doors
                     if (_openUnlitVisible && !_simpleVisuals)
                     {
                         sprite.LayerSetState(DoorVisualLayers.BaseUnlit, "open_unlit");
-                        sprite.LayerSetState(DoorVisualLayers.BaseBolted, "bolted_open_unlit");
-                        sprite.LayerSetState(DoorVisualLayers.BaseEmergencyAccess, "emergency_open_unlit");
                     }
                     break;
                 case DoorState.Closed:
@@ -194,7 +192,6 @@ namespace Content.Client.Doors
                     {
                         sprite.LayerSetState(DoorVisualLayers.BaseUnlit, "closed_unlit");
                         sprite.LayerSetState(DoorVisualLayers.BaseBolted, "bolted_unlit");
-                        sprite.LayerSetState(DoorVisualLayers.BaseEmergencyAccess, "emergency_unlit");
                     }
                     break;
                 case DoorState.Opening:
@@ -224,7 +221,6 @@ namespace Content.Client.Doors
             var boltedVisible = false;
             var emergencyLightsVisible = false;
             var unlitVisible = false;
-            var baseVisible = true;
 
             if (component.TryGetData(DoorVisuals.Powered, out bool powered) && powered)
             {
@@ -235,19 +231,15 @@ namespace Content.Client.Doors
                     || state == DoorState.Denying
                     || state == DoorState.Open && _openUnlitVisible
                     || (component.TryGetData(DoorVisuals.ClosedLights, out bool closedLights) && closedLights);
-
-                if ((state == DoorState.Open || state == DoorState.Closed) && (emergencyLightsVisible || boltedVisible))
-                    baseVisible = false;
             }
 
-            sprite.LayerSetVisible(DoorVisualLayers.BaseUnlit, unlitVisible && baseVisible);
-            sprite.LayerSetVisible(DoorVisualLayers.BaseBolted, unlitVisible && boltedVisible);
+            sprite.LayerSetVisible(DoorVisualLayers.BaseUnlit, unlitVisible);
+            sprite.LayerSetVisible(DoorVisualLayers.BaseBolted, boltedVisible);
             if (_emergencyAccessLayer)
             {
                 sprite.LayerSetVisible(DoorVisualLayers.BaseEmergencyAccess,
-                    unlitVisible
-                        && emergencyLightsVisible
-                        && !boltedVisible
+                        emergencyLightsVisible
+                        && state != DoorState.Open
                         && state != DoorState.Opening
                         && state != DoorState.Closing);
             }
