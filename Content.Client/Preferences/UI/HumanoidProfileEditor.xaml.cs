8d4b13ffd32852ec845db4502058c1de4c7084e5
Cooderer <31316111+Cooderer@users.noreply.github.com>
2023-05-23 19:08:00 +0300

    Ньюки по времени (#173)
    ¯\_( ͡° ͜ʖ ͡°)_/¯

--- Content.Client/Preferences/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Preferences/UI/HumanoidProfileEditor.xaml.cs
@@ -478,14 +478,23 @@ namespace Content.Client.Preferences.UI
 
             foreach (var antag in prototypeManager.EnumeratePrototypes<AntagPrototype>().OrderBy(a => Loc.GetString(a.Name)))
             {
+                bool check = false;
+                if(playTime.IsAllowed(antag, out var reason))
+                {
+                    check = true;
+                }
+                
                 if (!antag.SetPreference)
                 {
                     continue;
                 }
 
-                var selector = new AntagPreferenceSelector(antag);
+                var selector = new AntagPreferenceSelector(antag,check);
+                selector.Preference = false;
                 _antagList.AddChild(selector);
                 _antagPreferences.Add(selector);
+
+                
 
                 selector.PreferenceChanged += preference =>
                 {
@@ -1298,7 +1307,10 @@ namespace Content.Client.Preferences.UI
                 var antagId = preferenceSelector.Antag.ID;
                 var preference = Profile?.AntagPreferences.Contains(antagId) ?? false;
 
-                preferenceSelector.Preference = preference;
+                if (!preferenceSelector.Locked)
+                {
+                    preferenceSelector.Preference = preference;
+                }
             }
         }
 
@@ -1317,16 +1329,16 @@ namespace Content.Client.Preferences.UI
         {
             public AntagPrototype Antag { get; }
             private readonly CheckBox _checkBox;
-
+            
             public bool Preference
             {
                 get => _checkBox.Pressed;
                 set => _checkBox.Pressed = value;
             }
-
+            public bool Locked = false;
             public event Action<bool>? PreferenceChanged;
 
-            public AntagPreferenceSelector(AntagPrototype antag)
+            public AntagPreferenceSelector(AntagPrototype antag, bool check)
             {
                 Antag = antag;
 
@@ -1347,6 +1359,11 @@ namespace Content.Client.Preferences.UI
                         _checkBox
                     }
                 });
+                if (check == false)
+                {
+                    Locked = true;
+                    _checkBox.Disabled = true;
+                }
             }
 
             private void OnCheckBoxToggled(BaseButton.ButtonToggledEventArgs args)
