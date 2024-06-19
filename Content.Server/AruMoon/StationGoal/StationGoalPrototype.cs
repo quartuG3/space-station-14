04a1e14d70d805736f579d9ee0bc8adb8fa6fa60
lzk <124214523+lzk228@users.noreply.github.com>
2024-06-20 00:14:34 +0200

    Cherry-pick stationgoal update

--- Content.Server/AruMoon/StationGoal/StationGoalPrototype.cs
+++ Content.Server/AruMoon/StationGoal/StationGoalPrototype.cs
@@ -5,8 +5,17 @@ namespace Content.Server.AruMoon.StationGoal
     [Serializable, Prototype("stationGoal")]
     public sealed class StationGoalPrototype : IPrototype
     {
-        [IdDataField] public string ID { get; } = default!;
+        [IdDataFieldAttribute]
+        public string ID { get; } = default!;
 
-        [DataField("text")] public string Text { get; set; } = string.Empty;
+        [DataField]
+        public string Text { get; set; } = string.Empty;
+
+        [DataField]
+        public int? MinPlayers;
+
+        [DataField]
+        public int? MaxPlayers;
+
     }
 }
