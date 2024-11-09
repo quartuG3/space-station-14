6298a50f352afecd2193cea0a8ed581cdb4902ac
MilenVolf <milen.volf@gmail.com>
2024-11-10 01:02:02 +0300

    Use new syringe gun, instead of old one

--- Content.Shared/Projectiles/EmbeddableProjectileComponent.cs
+++ Content.Shared/Projectiles/EmbeddableProjectileComponent.cs
@@ -42,6 +42,12 @@ public sealed partial class EmbeddableProjectileComponent : Component
     public Vector2 Offset = Vector2.Zero;
 
     /// <summary>
+    /// Angle of the embed projectile on the entity.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public Angle Angle = Angle.Zero;
+
+    /// <summary>
     /// Sound to play after embedding into a hit target.
     /// </summary>
     [DataField, AutoNetworkedField]
